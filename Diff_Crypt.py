e03a46685ad5a3785f20cd85ef803ab43658da64
thekoc11 <thekoc11@gmail.com>
2019-04-25 16:29:05 +0530

    Key found

--- Diff_Crypt.py
+++ Diff_Crypt.py
@@ -103,12 +103,53 @@ class SPN:
 	Y = []
 	_X = []
 
-	def __init__(self, X):
+	u = []
+	v = []
+	w = []
+	v_w = []
+
+	def calcU(self, r, w_r_):
 		u = []
+		for j in range(len(w_r_)):
+			u.append(w_r_[j] ^ self.K[r][j])
+		self.U[r] = u
+
+	def calcV(self, r, u_r):
 		v = []
+		for j in range(len(u_r)):
+			v.append(SBox(u_r[j]))
+		self.V[r] = v
+
+	def calcW(self, r, v_r):
 		w = []
 		v_w = []
-		self._X = X
+		for v in v_r:
+			v_w.append(convertBin(v))
+
+		w_row = []
+		for j in range(4):
+			k = j*4
+			for m in range (k, k+4):
+				w_row.append(v_w[int(PBox(m)/4)][PBox(m)%4])
+			w.append(w_row)
+			w_row = []
+		for j in range(4):
+			w[j] = BackToInt(w[j])
+			# print("part w",i, w[j])
+
+		self.W[r] = w
+
+	def calcY(self):
+		y = []
+		l = len(self.V)
+		k = len(self.K)
+		for j in range(len(self.V[l-1])):
+			y.append(self.V[l-1][j] ^ self.K[l-1][j])
+			# print("part y", w[j])
+		self.Y = y
+
+
+	def __init__(self, X):
 		for i in range(5):
 			rkey = []
 			for j in range(i, i+4):
@@ -116,6 +157,7 @@ class SPN:
 			# print(rkey)
 			self.K.append(rkey)
 		for x in X:
+			self._X = x
 			if len(x) != len(self.K[1]):
 				print("invalid input")
 				pass
@@ -123,52 +165,57 @@ class SPN:
 				# W[0] = x
 				for i in range(0, 4):
 					if i is 0:
-						for j in range(4):
-							u.append(x[j] ^ self.K[0][j])
-							# print("part u0", u[j])
-						self.U[i] = u
-						u = []
+						self.calcU(i, x)
+						# for j in range(4):
+						# 	u.append(x[j] ^ self.K[0][j])
+						# 	# print("part u0", u[j])
+						# self.U[i] = u
+						# u = []
 					else:
-						for j in range(4):
-							u.append(self.W[i-1][j] ^ self.K[i][j])
-							# print("part u",i, u[j])
-						self.U[i] = u
-						u = []
+						self.calcU(i, self.W[i-1])
+						# for j in range(4):
+						# 	u.append(self.W[i-1][j] ^ self.K[i][j])
+						# 	# print("part u",i, u[j])
+						# self.U[i] = u
+						# u = []
 
-					for j in range(4):
-						v.append(SBox(self.U[i][j]))
-						v_w.append(convertBin(v[j]))
-						# print("part v",i, v[j])
+					self.calcV(i, self.U[i])
+					# for j in range(4):
+					# 	v.append(SBox(self.U[i][j]))
+					# 	v_w.append(convertBin(v[j]))
+					# 	# print("part v",i, v[j])
 
-					self.V[i] = v
-					v = []
+					# self.V[i] = v
+					# v = []
 
 					w_row = []
 					# _w_ = 0
 					# wmod = 0
 					if i != 3:
-						for j in range(4):
-							# _w_ = v[int(PBox(self.V[i][j])/4)]
-					# 		wmod = (int(PBox(self.V[i][j]) % 4))
-					# 		print("wmod", wmod)
-							k = j*4
-							for m in range (k, k+4):
-								w_row.append(v_w[int(PBox(m)/4)][PBox(m)%4])
-							w.append(w_row)
-							w_row = []
-						for j in range(4):
-							w[j] = BackToInt(w[j])
-							# print("part w",i, w[j])
+						self.calcW(i, self.V[i])
+					# 	for j in range(4):
+					# 		# _w_ = v[int(PBox(self.V[i][j])/4)]
+					# # 		wmod = (int(PBox(self.V[i][j]) % 4))
+					# # 		print("wmod", wmod)
+					# 		k = j*4
+					# 		for m in range (k, k+4):
+					# 			w_row.append(v_w[int(PBox(m)/4)][PBox(m)%4])
+					# 		w.append(w_row)
+					# 		w_row = []
+					# 	for j in range(4):
+					# 		w[j] = BackToInt(w[j])
+					# 		# print("part w",i, w[j])
 
-						self.W[i] = w
-						w = []
-						v_w = []
+					# 	self.W[i] = w
+					# 	w = []
+					# 	v_w = []
 					else:
-						for j in range(4):
-							w.append(self.V[i][j] ^ self.K[i+1][j])
-							# print("part y", w[j])
-						self.Y = w
-						w = []
+						self.calcY()
+						# for j in range(4):
+						# 	w.append(self.V[i][j] ^ self.K[i+1][j])
+						# 	# print("part y", w[j])
+						# self.Y = w
+						# w = []
 						
 	def display(self):
 		# W_disp = [bin(_X), bin(W[0]), bin(W[1]), bin(W[2])]
@@ -215,11 +262,10 @@ class SPN:
 			y_d.append(convertBin(y))
 		Y_disp.append(y_d)
 		X_disp = []
+		x_d = []
 		for x in self._X:
-			x_d = []
-			for i in range(len(x)):
-				x_d.append(convertBin(x[i]))
-			X_disp.append(x_d)
+			x_d.append(convertBin(x))
+		X_disp.append(x_d)
 
 		for w in W_disp:
 			print("W is", w)
@@ -239,47 +285,67 @@ class SPN:
 		print("K[3] is ", K3_disp)
 		print("K[4] is ", K4_disp)
 
-	def DiffAttack(self, T, Pi_inv):
+def DiffAttack(T, Pi_inv):
 
-		count = []
-		for l1 in range(0, 16):
-			countRow = []
-			for l2 in range(0, 16):
-				countRow.append(0)
-			count.append(countRow)
-			# print (countRow)
+	count = []
+	for l1 in range(0, 16):
+		countRow = []
+		for l2 in range(0, 16):
+			countRow.append(0)
+		count.append(countRow)
+		# print (countRow)
 
-		for [x, y, x_, y_] in T:
-			if y[0] == y_[0] and y[2] == y_[2]:
-				for l1 in range(0, 16):
-					for l2 in range(0, 16):
-						v42 = l1 ^ y[1]
-						v44 = l2 ^ y[3]
-						u42 = Pi_inv[v42]
-						u44 = Pi_inv[v44]
-						v42_ = l1 ^ y_[1]
-						v44_ = l2 ^ y_[3]
-						u42_ = Pi_inv[v42_]
-						u44_ = Pi_inv[v44_]
-						u42Prime = u42 ^ u42_
-						u44Prime = u44 ^ u44_
-						# print(u42Prime == 0b0110 and u44Prime == 0b0110)
-						if u42Prime == 0b0110 and u44Prime == 0b0110:
-							# print(u42Prime)
-							count[l1][l2] = count[l1][l2] + 1
+	for [sp, sp_] in T:
+		x = sp._X
+		x_ = sp_._X
+		y = sp.Y
+		y_ = sp_.Y
+		u42 = 0
+		u42_ = 0
+		u44 = 0
+		u44_ = 0
+		# print(y[0] == y_[0] and y[2] == y_[2])
+		if y[0] == y_[0] and y[2] == y_[2]:
+			for l1 in range(0, 16):
+				for l2 in range(0, 16):
+					v42 = l1 ^ y[1] 
+					v44 = l2 ^ y[3]
+					u42 = Pi_inv[v42]
+					u44 = Pi_inv[v44]
+						# print("u42", u42, l1, l2)
+					v42_ = l1 ^ y_[1] 
+					v44_ = l2 ^ y_[3]					
+					u42_ = Pi_inv[v42_]
+					u44_ = Pi_inv[v44_]
+						# print("u42*", u42_)
+					u42Prime = u42 ^ u42_
+					u44Prime = u44 ^ u44_
+					# print(u42Prime , u44Prime )
+					if u42Prime == 0b0110 and u44Prime == 0b0110:
+						# print("True")
+						count[l1][l2] = count[l1][l2] + 1
 
-		_max = -1
-		# maxkey = [0, 0]
+	_max = -1
+	# maxkey = [0, 0]
 
-		for l1 in range(0, 16):
-			for l2 in range(0, 16):
-				if count[l1][l2] > _max:
-					_max = count[l1][l2]
-					maxkey = [l1, l2]
+	for l1 in range(0, 16):
+		for l2 in range(0, 16):
+			if count[l1][l2] > _max:
+				_max = count[l1][l2]
+				print(l1, l2, float(_max/256.0))
+				maxkey = [l1, l2]
 
-		maxkey_disp = [bin(maxkey[0]), bin(maxkey[1])]
-		print (maxkey_disp)
-		# print (T)
+	maxkey_disp = [bin(maxkey[0]), bin(maxkey[1])]
+	print (maxkey)
+	# for [sp, sp_] in T:
+	# 	x = sp._X
+	# 	x_ = sp_._X
+	# 	y = sp.Y
+	# 	y_ = sp_.Y
+	# 	if sp.V[3][0] == y[0] ^ maxkey[0] and sp.V[3][2] == y[2] ^ maxkey[1]:
+	# 		print(sp.V[3][1]^y[1], sp.V[3][3] ^ y[3])
+
+	# print (T)
 
 
 
@@ -385,7 +451,7 @@ for i in range(len(X)):
 	t = [x, y, x_, y_]
 	T.append(t)
 
-print(len(X))
+# print(len(X))
 
 
 # print(YStar)
@@ -393,38 +459,80 @@ print(len(X))
 # print(yStarbin)
 # print(yPrimeBin)
 
-INP = []
+INP = []###################### x
 for i in range(16):
+	for j in range(16):
+		for k in range(16):
 	# print ("x",[0, i, 0, 0] )
-	INP.append([0, i, 0, 0])
+			INP.append([0, i, j, k])
 
-sp = []
-for i in range(16):
+sp = []######################## x
+for i in range(len(INP)):
 	sp.append(SPN([INP[i]]))
-t1 = sp[10].W[0]
+t1 = sp[10].U[0]
 print(t1)
-INP1 = []
+INP1 = []####################### x*
 for i in range(16):
+	for j in range(16):
+		for k in range(16):
 	# print ("x*", [0, DeltaX_[11][i], 0, 0] )
-	INP1.append([0, DeltaX_[11][i], 0, 0])
-sp1 = []
-for i in range(16):
+			INP1.append([0, DeltaX_[11][i], DeltaX_[0][j], DeltaX_[0][k]])
+sp1 = []######################### x*
+for i in range(len(INP1)):
 	sp1.append(SPN([INP1[i]]))
-t2 = sp1[10].W[0]
+t2 = sp1[10].U[0]
 print(t2)
 
+OP = []######################### y
+for i in range(len(INP)):
+	OP.append(sp[i].Y)
+
+
+OP1 = []######################### y*
+for i in range(len(INP1)):
+	OP.append(sp1[i].Y)
 # print(Y_Prime[8][3])
 for i in range(len(t1)):
 	print(t1[i] ^ t2[i])
 
+T =[]
+for i in range(len(sp)):
+	T.append([sp[i], sp1[i]])
+
+O_XOR = []
+I_XOR = []
+for i in range(len(sp)):
+	o = []
+	i_ = []
+	for j in range(len(sp[i].Y)):
+		o.append(sp[i].V[2][j] ^ sp1[i].V[2][j])
+		i_.append(sp[i]._X[j] ^ sp1[i]._X[j])
+	# print("Output XOR", i,  o, "Input XOR", i_)
+	O_XOR.append(o)
+	I_XOR.append(i_)
+
+# sp1[1].display()
+# sp[1].display()	
+
+i_x = 2
+prob = []
+for i in range(16):
+	prob.append(Rp(i_x, i))
+
+print(prob)
+print(prob.index(max(prob)))
+
 I = [[0, 11, 0, 0]]
 sp3 = SPN(I)
-t3 = sp3.V[0]
-print(t3)
+# sp3.display()
+# print(Rp(11, 2))
 # for i in range(len(sp.U[0])):
 	# print(sp.U[0] )#^ sp1.U[0][i])
-# sp[3].display()
-# DiffAttack(T, Pi_inv)
+sp3.V[2] = [0, 0, 6, 0]
+sp3.calcW(2, sp3.V[2])
+print(sp3.W[2])
+sp3.U[2] = sp3.W[1]
+DiffAttack(T, Pi_inv)
 # ___l = convertBin(15)
 # print(___l)
 # print(BackToInt(___l))
