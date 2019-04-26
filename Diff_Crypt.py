70fe241fc47c5c155563f00e11213beb9d62eb91
thekoc11 <thekoc11@gmail.com>
2019-04-26 07:45:03 +0530

    Bugs Fixed, Code Finalized

--- Diff_Crypt.py
+++ Diff_Crypt.py
@@ -1,4 +1,13 @@
-def SBox(n):
+"""
+
+Differential Cryptanalysis of SPN
+Author: Abhishek Nandekar
+
+"""
+
+################################# BEGIN ##########################################
+
+def SBox(n): # The implemention of the SBox function: n in an integer between 0 and 15 
 	val = hex(n)
 	
 	if val == '0x0':		
@@ -34,14 +43,12 @@ def SBox(n):
 	elif val == '0xf':
 		return int(0x7)
 
-def PBox(n): 
+def PBox(n): #Implementation of the PBox function. Again, n in an integer between 0 and 15
 
 
 	val = n + 1
 	retval = 0
-	# print("inside pbox n is", n)
 	if val == 1:
-		
 		retval = 1
 	elif val == 2:
 		retval = 5
@@ -76,54 +83,62 @@ def PBox(n):
 
 	return retval-1
 
-def convertBin(n):
+def convertBin(n): #takes an integer between 0 and 15 as input and converts it to an array of zeroes and ones, representing the binary notation of that number
 	p = format(n, '#06b')
 	p = p[2:]
 	p = [int (x) for x in p]
 	return p
-def BackToInt(l):
+def BackToInt(l):#takes an array of size 4 of zeroes and ones, and converts it to integer
 	r = 0
 	for item in l:
 		r = (r<<1)|item
 	return r
 
-def DeltaX(xp):
+def DeltaX(xp):#takes an integer x' in [0, 15] as input, and calculates all possible x*'s corresponding to x's in [0, 15]
 	delX = []
 	for i in range(16):
 		delX.append(i ^ xp)
-		# print("X and Xstar", i, delX[i])
 	return delX
+
+"""
+The SPN class
+
+"""
 class SPN:
 
-	K = []
-	Key = [3, 10, 9, 4, 13, 6, 3, 15]
+	K = [[], [], [], [], []]#5 round keys for an SPN of 4 rounds
+	Key = [3, 10, 9, 4, 13, 6, 3, 15]#the 32-bit key of the SPN
 	U = [[], [], [], []]
 	V = [[], [], [], []]
 	W = [[], [], []]
-	Y = []
-	_X = []
+	Y = []#SPN output
+	_X = []#SPN input
 
+#The following lists have been initialised just for the ease of calculation, and are not relevent since they do not correspond to any significant SPN property
 	u = []
 	v = []
 	w = []
 	v_w = []
 
-	def calcU(self, r, w_r_):
+	def calcU(self, r, w_r_):#calculates u_r for the round r, given the value of w_{r-1}
 		u = []
 		for j in range(len(w_r_)):
 			u.append(w_r_[j] ^ self.K[r][j])
 		self.U[r] = u
 
-	def calcV(self, r, u_r):
+	def calcV(self, r, u_r):#calculates v_r for the round r, given the value of u_r
 		v = []
 		for j in range(len(u_r)):
 			v.append(SBox(u_r[j]))
 		self.V[r] = v
 
-	def calcW(self, r, v_r):
+	def calcW(self, r, v_r):#calculates w_r for the round r, given the value of v_r
 		w = []
 		v_w = []
 		for v in v_r:
+		#We have represented all the 16-bit SPN parameters as an array of 4 integers in [0, 15]. 
+		#But to calculate w, we need all the 16 bits. Hence, here the required conversion is done
+
 			v_w.append(convertBin(v))
 
 		w_row = []
@@ -135,90 +150,59 @@ class SPN:
 			w_row = []
 		for j in range(4):
 			w[j] = BackToInt(w[j])
-			# print("part w",i, w[j])
 
 		self.W[r] = w
 
-	def calcY(self):
+	def calcY(self):#calculates the output Y of the SPN
 		y = []
 		l = len(self.V)
 		k = len(self.K)
 		for j in range(len(self.V[l-1])):
-			y.append(self.V[l-1][j] ^ self.K[l-1][j])
-			# print("part y", w[j])
+			y.append(self.V[l-1][j] ^ self.K[4][j])
 		self.Y = y
 
 
 	def __init__(self, X):
-		for i in range(5):
+		"""
+		#Initialises the SPN using the given input X. 
+		We expect a list* conataining a single list of size 4
+
+		* : (yes, a list, with a single element in it.... I had something else in my mind when creating this architecture, 
+			but ended up doing something completely different)  
+		"""
+		for i in range(5):#Generates 5 round keys for the SPN of 4 rounds
 			rkey = []
 			for j in range(i, i+4):
 				rkey.append(self.Key[j])
-			# print(rkey)
-			self.K.append(rkey)
-		for x in X:
+			self.K[i] = rkey
+
+		for x in X:#This loop runs only for one iteration since X contains just one element
 			self._X = x
 			if len(x) != len(self.K[1]):
 				print("invalid input")
 				pass
 			else:
-				# W[0] = x
+				"""
+					Since we are using arrays here, we are forced to used zero-indexing.
+					So, here U0 will denote the SBox inputs for the first round.
+					This also means that W0 here is the actual W1. 
+					Instead of creating an actual W0, we've directly calculated the actual U1's using the input X
+				"""
 				for i in range(0, 4):
 					if i is 0:
 						self.calcU(i, x)
-						# for j in range(4):
-						# 	u.append(x[j] ^ self.K[0][j])
-						# 	# print("part u0", u[j])
-						# self.U[i] = u
-						# u = []
 					else:
 						self.calcU(i, self.W[i-1])
-						# for j in range(4):
-						# 	u.append(self.W[i-1][j] ^ self.K[i][j])
-						# 	# print("part u",i, u[j])
-						# self.U[i] = u
-						# u = []
 
 					self.calcV(i, self.U[i])
-					# for j in range(4):
-					# 	v.append(SBox(self.U[i][j]))
-					# 	v_w.append(convertBin(v[j]))
-					# 	# print("part v",i, v[j])
 
-					# self.V[i] = v
-					# v = []
-
-					w_row = []
-					# _w_ = 0
-					# wmod = 0
-					if i != 3:
+					if i != 3:#Calculate W's except for the last round
 						self.calcW(i, self.V[i])
-					# 	for j in range(4):
-					# 		# _w_ = v[int(PBox(self.V[i][j])/4)]
-					# # 		wmod = (int(PBox(self.V[i][j]) % 4))
-					# # 		print("wmod", wmod)
-					# 		k = j*4
-					# 		for m in range (k, k+4):
-					# 			w_row.append(v_w[int(PBox(m)/4)][PBox(m)%4])
-					# 		w.append(w_row)
-					# 		w_row = []
-					# 	for j in range(4):
-					# 		w[j] = BackToInt(w[j])
-					# 		# print("part w",i, w[j])
-
-					# 	self.W[i] = w
-					# 	w = []
-					# 	v_w = []
 					else:
 						self.calcY()
-						# for j in range(4):
-						# 	w.append(self.V[i][j] ^ self.K[i+1][j])
-						# 	# print("part y", w[j])
-						# self.Y = w
-						# w = []
+
 						
-	def display(self):
-		# W_disp = [bin(_X), bin(W[0]), bin(W[1]), bin(W[2])]
+	def display(self):#function that displays all the SPN parameters, over all the 4 rounds
 		K0_disp = []
 		K1_disp = []
 		K2_disp = []
@@ -228,6 +212,7 @@ class SPN:
 		V_disp = []
 		U_disp = []
 		Y_disp = []
+		X_disp = []
 
 		for i in range(4):
 			K0_disp.append(convertBin(self.K[0][i]))
@@ -236,32 +221,29 @@ class SPN:
 			K3_disp.append(convertBin(self.K[3][i]))
 			K4_disp.append(convertBin(self.K[4][i]))
 
-		# for i in range(5):
 		for w in self.W:
 			w_d = []
-			# print(len(w))
 			for i in range(len(w)):
 				w_d.append(convertBin(w[i]))
 			W_disp.append(w_d)
+
 		for u in self.U:
 			u_d = []
-			# print(len(w))
 			for i in range(len(u)):
 				u_d.append(convertBin(u[i]))
 			U_disp.append(u_d)
+
 		for v in self.V:
 			v_d = []
-			# print(len(w))
 			for i in range(len(v)):
 				v_d.append(convertBin(v[i]))
 			V_disp.append(v_d)
 
 		y_d = []
-
 		for y in self.Y:
 			y_d.append(convertBin(y))
 		Y_disp.append(y_d)
-		X_disp = []
+
 		x_d = []
 		for x in self._X:
 			x_d.append(convertBin(x))
@@ -285,15 +267,23 @@ class SPN:
 		print("K[3] is ", K3_disp)
 		print("K[4] is ", K4_disp)
 
-def DiffAttack(T, Pi_inv):
+################################################## The Differential Attack ###################################################
 
+def DiffAttack(T, Pi_inv):
+	"""
+		In this function, the tuple T consists of two objects of the SPN class, one corresponding to the input x and other corresponding to the input x*
+		We assume no access to any parameter of the SPN, except the input x and the output y. 
+	"""
 	count = []
+
 	for l1 in range(0, 16):
+		"""
+			We want to find the 8-bit candidate key for a given differential trail. This 8-bit candidate forms half of the key in round 5
+		"""
 		countRow = []
 		for l2 in range(0, 16):
 			countRow.append(0)
 		count.append(countRow)
-		# print (countRow)
 
 	for [sp, sp_] in T:
 		x = sp._X
@@ -304,7 +294,6 @@ def DiffAttack(T, Pi_inv):
 		u42_ = 0
 		u44 = 0
 		u44_ = 0
-		# print(y[0] == y_[0] and y[2] == y_[2])
 		if y[0] == y_[0] and y[2] == y_[2]:
 			for l1 in range(0, 16):
 				for l2 in range(0, 16):
@@ -312,79 +301,54 @@ def DiffAttack(T, Pi_inv):
 					v44 = l2 ^ y[3]
 					u42 = Pi_inv[v42]
 					u44 = Pi_inv[v44]
-						# print("u42", u42, l1, l2)
 					v42_ = l1 ^ y_[1] 
 					v44_ = l2 ^ y_[3]					
 					u42_ = Pi_inv[v42_]
 					u44_ = Pi_inv[v44_]
-						# print("u42*", u42_)
 					u42Prime = u42 ^ u42_
 					u44Prime = u44 ^ u44_
-					# print(u42Prime , u44Prime )
 					if u42Prime == 0b0110 and u44Prime == 0b0110:
-						# print("True")
 						count[l1][l2] = count[l1][l2] + 1
 
 	_max = -1
-	# maxkey = [0, 0]
 
+	print("\nThe candidate keys with their probabilities are (count, probability, l1, l2)")
 	for l1 in range(0, 16):
 		for l2 in range(0, 16):
 			if count[l1][l2] > _max:
 				_max = count[l1][l2]
-				print(l1, l2, float(_max/256.0))
+				print(_max, float(_max/256.0), l1, l2)
 				maxkey = [l1, l2]
 
 	maxkey_disp = [bin(maxkey[0]), bin(maxkey[1])]
+	print("\nHence the most probable key candidates for S42 and S44 are:")
 	print (maxkey)
-	# for [sp, sp_] in T:
-	# 	x = sp._X
-	# 	x_ = sp_._X
-	# 	y = sp.Y
-	# 	y_ = sp_.Y
-	# 	if sp.V[3][0] == y[0] ^ maxkey[0] and sp.V[3][2] == y[2] ^ maxkey[1]:
-	# 		print(sp.V[3][1]^y[1], sp.V[3][3] ^ y[3])
 
-	# print (T)
-
-
+	print("The original key for the final round(K5) is:")
+	print(sp.K[len(sp.K) - 1])
+	print("\n Congratulations!!! The Differential Attack was successful")
 
 ##################### FINAL Algorithm ################################
-X_prime = [] #constant x', change here to get a different key value
+X_prime = [] 
 
 for i in range(0, 16):
 	X_prime.append(i)
 
-DeltaX_ = []#both x and x* in this dict
-# X = [[0, 0, 0, 0]]#this stands for 0000 0000 0000 0000
+DeltaX_ = []#A 2d array, such that DeltaX_[x'][x] = x*
 X =  []
 for i in range(0, 16):
 	DeltaX_.append(DeltaX(i))
-
-for i in range(0, 16):
-	print("DeltaX_ for X' = 11", DeltaX_[i][11])	
 		
 for i in range(0, 16):
 	X.append(i)
 		
-# print ("\n")
 XStar = []
-# for i in range(0, 16):
-# 	_x = [X_XStar[0][i], X_XStar[1][i], X_XStar[2][i], X_XStar[3][i]]
-# 	XStar.append(_x)
 
 for x in X:
 	x_starRow = []
 	for xp in X_prime:
 		x_starRow.append(DeltaX_[x][xp])
 	XStar.append(x_starRow)
-
-print(XStar[11])
-# for x in X:
-# 	print (x[1], "\n")
-
-
-
 
 Y = []
 YStar = []
@@ -399,8 +363,6 @@ for x_ in XStar:
 		yrow.append(SBox(x_[j]))
 	YStar.append(yrow)
 
-print(YStar[11])
-
 Y_Prime = []
 
 for y in YStar:
@@ -408,8 +370,6 @@ for y in YStar:
 	for j in range(16):
 		yrow.append(Y[j] ^ y[j])
 	Y_Prime.append(yrow)
-
-print(Y_Prime[11])
 
 b_prime = []
 
@@ -420,68 +380,43 @@ for y in Y_Prime:
 		brow.append(c)
 	b_prime.append(brow)
 
-for b in b_prime:
-	print(b)
 
 Nd = []
 for b in b_prime:
 	ndrow = []
 	for j in range(16):
 		ndrow.append(float(b[j]))
-	print(ndrow)
 	Nd.append(ndrow)
 		
-def Rp(i, j):
+def Rp(i, j):#Function that calculates the propogation ratio
 	return(Nd[i][j]/16)
-# for x in Y:
-# 	print ("Y", x[1], "\n")
-# print(YStar)
-Pi_inv = {0:0}
+
+Pi_inv = {0:0}#Dictionary that stores the inverse SBox values
 
 for i in range(0, 16):
 	Pi_inv[SBox(i)] = i
 
-T = []
-
-for i in range(len(X)):
-	x = X[i]
-	x_ = XStar[i]
-	y = Y[i]
-	y_ = YStar[i]
-	t = [x, y, x_, y_]
-	T.append(t)
-
-# print(len(X))
-
-
-# print(YStar)
-# print(Pi_inv)
-# print(yStarbin)
-# print(yPrimeBin)
 
 INP = []###################### x
 for i in range(16):
 	for j in range(16):
 		for k in range(16):
-	# print ("x",[0, i, 0, 0] )
 			INP.append([0, i, j, k])
 
-sp = []######################## x
+sp = []######################## array of SPN's for all possible values x
+
 for i in range(len(INP)):
 	sp.append(SPN([INP[i]]))
-t1 = sp[10].U[0]
-print(t1)
+
 INP1 = []####################### x*
 for i in range(16):
 	for j in range(16):
 		for k in range(16):
-	# print ("x*", [0, DeltaX_[11][i], 0, 0] )
 			INP1.append([0, DeltaX_[11][i], DeltaX_[0][j], DeltaX_[0][k]])
-sp1 = []######################### x*
+
+sp1 = []#########################array of SPN's for all possible values of x*
 for i in range(len(INP1)):
 	sp1.append(SPN([INP1[i]]))
-t2 = sp1[10].U[0]
-print(t2)
 
 OP = []######################### y
 for i in range(len(INP)):
@@ -491,48 +426,52 @@ for i in range(len(INP)):
 OP1 = []######################### y*
 for i in range(len(INP1)):
 	OP.append(sp1[i].Y)
-# print(Y_Prime[8][3])
-for i in range(len(t1)):
-	print(t1[i] ^ t2[i])
 
-T =[]
+T =[] # Tuples to be used in the Differential Attack
 for i in range(len(sp)):
 	T.append([sp[i], sp1[i]])
 
-O_XOR = []
-I_XOR = []
-for i in range(len(sp)):
-	o = []
-	i_ = []
-	for j in range(len(sp[i].Y)):
-		o.append(sp[i].V[2][j] ^ sp1[i].V[2][j])
-		i_.append(sp[i]._X[j] ^ sp1[i]._X[j])
-	# print("Output XOR", i,  o, "Input XOR", i_)
-	O_XOR.append(o)
-	I_XOR.append(i_)
+print("The dataset size is: ",  len(INP)) #Size of the input dataset
 
-# sp1[1].display()
-# sp[1].display()	
+def calcMaxProbV(x):#Returns the value that is most likely to occur looking at the propogation ratios for a given value of x
+	prob = []
+	retval = 0
+	for i in range(16):
+		prob.append(Rp(x, i))
 
-i_x = 2
-prob = []
-for i in range(16):
-	prob.append(Rp(i_x, i))
+	retval = prob.index(max(prob))
+	return [retval, max(prob)]
 
-print(prob)
-print(prob.index(max(prob)))
+testsp = SPN([[0, 11, 0, 0]])#Constant x' is 0000 1011 0000 0000
 
-I = [[0, 11, 0, 0]]
-sp3 = SPN(I)
-# sp3.display()
-# print(Rp(11, 2))
-# for i in range(len(sp.U[0])):
-	# print(sp.U[0] )#^ sp1.U[0][i])
-sp3.V[2] = [0, 0, 6, 0]
-sp3.calcW(2, sp3.V[2])
-print(sp3.W[2])
-sp3.U[2] = sp3.W[1]
+print("\nThe Differential Trail: ")
+p = 0
+for i in range(3):
+	if i is 0:
+		testsp.U[i] = testsp._X
+		arr = testsp.U[i]
+		for j in range(len(arr)):
+			testsp.V[i][j] = calcMaxProbV(arr[j])[0]
+			if arr[j] is not 0:
+				p = calcMaxProbV(arr[j])[1]
+			# print(p)
+		
+		testsp.calcW(i, testsp.V[i])
+	else:
+		testsp.U[i] = testsp.W[i-1]
+		arr = testsp.U[i]
+		for j in range(len(arr)):
+			testsp.V[i][j] = calcMaxProbV(arr[j])[0]
+			if arr[j] is not 0:
+				p *= calcMaxProbV(arr[j])[1]
+		
+		if i is not 3:
+			testsp.calcW(i, testsp.V[i])
+
+	print("Round", i+1, "U", testsp.U[i])
+	print("Round", i+1, "V", testsp.V[i])
+	if i is not 3:	
+		print("Round", i+1, "W", testsp.W[i])
+	print("probability", p)
+
 DiffAttack(T, Pi_inv)
-# ___l = convertBin(15)
-# print(___l)
-# print(BackToInt(___l))
